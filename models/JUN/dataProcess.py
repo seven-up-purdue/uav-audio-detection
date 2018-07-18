4bf3c223728c10b140925c3c2092d05d9e8ac00e
JunHyuckWoo <wjh0470@gmail.com>
2018-07-18 14:39:08 -0400

    Modify data processing class

--- models/JUN/dataProcess.py
+++ models/JUN/dataProcess.py
@@ -4,20 +4,26 @@ import glob
 
 class Sound(object):
 
-    raw = []
+    # Variable
+    #raw = []    # For raw data
 
     # Constructor
     def __init__(self, path = None, extension = None):
+        self.raw = []
         self.path = path            # Get data path
         self.extension = extension  # Get data extension
+        self.dataNames = glob.glob(self.path + "*." + self.extension)
         print(self.path)        # For debugging
         print(self.extension)   # For debugging
 
     # Load raw data & attach to one chunk
     def load(self):
-        self.dataNames = glob.glob(self.path + "*." + self.extension)
         for i in range(self.dataNames.__len__()):
             self.raw.append(librosa.load(self.dataNames.pop(0)))
         print(self.dataNames)   # For debugging
         print(self.raw)         # For debugging
-        
+
+    # process data
+    # Method:
+    def process(self):
+        return
