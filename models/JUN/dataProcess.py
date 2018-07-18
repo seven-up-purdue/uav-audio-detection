7a000eaa2bdb01fd3f32f80add9fd6f8e4e485cc
JunHyuckWoo <wjh0470@gmail.com>
2018-07-18 09:47:47 -0400

    Finish load function

--- models/JUN/dataProcess.py
+++ models/JUN/dataProcess.py
@@ -1,12 +1,23 @@
 import librosa
+import glob
 
 
 class Sound(object):
 
+    raw = []
+
     # Constructor
-    # Get data path
     def __init__(self, path = None, extension = None):
-        self.path = path
-        self.extension = extension
-        print(self.path)
-        print(self.extension)
+        self.path = path            # Get data path
+        self.extension = extension  # Get data extension
+        print(self.path)        # For debugging
+        print(self.extension)   # For debugging
+
+    # Load raw data & attach to one chunk
+    def load(self):
+        self.dataNames = glob.glob(self.path + "*." + self.extension)
+        for i in range(self.dataNames.__len__()):
+            self.raw.append(librosa.load(self.dataNames.pop(0)))
+        print(self.dataNames)   # For debugging
+        print(self.raw)         # For debugging
+        
