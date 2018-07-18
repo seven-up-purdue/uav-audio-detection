b430a9456f5d56f7e83d041ad256a8e523c77ba4
JunHyuckWoo <wjh0470@gmail.com>
2018-07-17 22:26:35 -0400

    Modify constructor

--- models/JUN/dataProcess.py
+++ models/JUN/dataProcess.py
@@ -5,7 +5,8 @@ class Sound(object):
 
     # Constructor
     # Get data path
-    def __init__(self, path, extension):
+    def __init__(self, path = None, extension = None):
         self.path = path
         self.extension = extension
         print(self.path)
+        print(self.extension)
