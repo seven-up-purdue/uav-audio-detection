fb58a19daa960a5dcdbf6e0f224c1b53de1ae0e5
JunHyuckWoo <wjh0470@gmail.com>
2018-07-20 18:27:21 -0400

    Wright few comment

--- models/JUN/testMain.py
+++ models/JUN/testMain.py
@@ -1,8 +1,38 @@
-
+from dataProcess import Sound
 
 def main():
+    load = Sound("../../soundData/0720A/load/", "wav")
+    load.load()
+    unload = Sound("../../soundData/0720A/unload/", "wav")
+    unload.load()
+
+
     return
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+"""
+data1: get from Wright state univ
+- load: ../../soundData/0701A/load/
+- unload: ../../soundData/0701A/unload/
+
+data2: make in Armory (Master student)
+- load: ../../soundData/0720A/load/
+- unload: ../../soundData/0720A/unload/
+
+data3: make in Armory (Notebook)
+- load: ../../soundData/0720B/load/
+- unload: ../../soundData/0720B/unload/ 
+
+data4: make in Armory (Raspberry)
+- load: ../../soundData/0720C/load/
+- unload: ../../soundData/0720C/unload/
+
+"""
