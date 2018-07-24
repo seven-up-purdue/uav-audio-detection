552ec3a54edfa76d5d1062a516011ca6ac3f7ced
JunHyuckWoo <wjh0470@gmail.com>
2018-07-23 20:17:21 -0400

    Divide preprocess function to dataCutting and preProcess For cut data as Sampling rate multiple

--- models/JUN/dataProcess.py
+++ models/JUN/dataProcess.py
@@ -1,6 +1,6 @@
 import librosa
 import glob
-
+#import matplotlib.pyplot as plt
 
 class Sound(object):
 
@@ -11,34 +11,45 @@ class Sound(object):
         self.extension = extension  # Get data extension
         self.dataNames = glob.glob(self.path + "*." + self.extension)  # Make name package
         self.dataNum = self.dataNames.__len__() # Get data number
-        print(self.path)        # For debugging
-        print(self.extension)   # For debugging
+        print("Data path: ", self.path)        # For debugging
+        print("Extension: ", self.extension)   # For debugging
+        print("Data number: ", self.dataNum)   # For debugging
 
     # Load raw data & attach to one chunk
     def load(self):
         for i in range(self.dataNum):
             buf, self.sr = librosa.load(self.dataNames.pop(0))  # Get raw data and sampling rate
             self.raw.append(buf)    # Collect data as one array
-        print(self.dataNames)   # For debugging
-        print(self.raw)         # For debugging
+        print("Data names: ", self.dataNames)   # For debugging
+        print("Raw data set: ", self.raw)       # For debugging
+        print("Sampling Rate: ", self.sr)       # For debugging
 
-    # Preprocess for get feature of data
     # Cut data using sampling rate
-    def preProcess(self):
+    def dataCutting(self):
+        self.cutSound = []
+        # data is cut by Sampling Rate multiple
         for i in range(self.dataNum):
             tmp = len(self.raw[i])
             cutting = 0
-            while tmp < self.sr:
+            while tmp > self.sr:
                 tmp -= self.sr
                 cutting += self.sr
-            self.base.append(self.raw[i][:cutting])
+            self.cutSound.append(self.raw[i][:cutting])
+            self.mfcc.append(librosa.feature.mfcc(self.cutSound[i]))
+            print(i, "Cut sound is made")
+            print("cutSound[", i, "]shape: ", self.cutSound[i].shape)
+            print(i, " MFCC is made")
+            print("mfcc[", i, "] shape: ", self.mfcc[i].shape)
 
-
+    # Preprocess for get feature of data
+    def preProcess(self):
+        self.mfcc = [] # Feature data
 
     # process data
     # Method:
     def process(self, method):
         if method == "MFCC" or "mfcc":
             mfcc = librosa.feature.mfcc(self.raw[0], self.sr)
-
-            return
+            return mfcc
+        else:
+            return None
